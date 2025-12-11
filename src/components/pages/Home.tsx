4d0f5d18ca25e6db8d40897f59460f71b45c3b89
Lorenzo <lorenzoferritsic@gmail.com>
2025-12-11 11:07:01 -0500

    Fixed CSS on landing page, added tailwind

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,17 +6,37 @@ import viteLogo from '../../assets/vite.svg'
 const Header = () => {
   const [count, setCount] = useState(0)
   return (
-     <main>   
-        <div>
-            <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+     <main>
+        <div id="landingPageBox">
+            <div className="titleDiv textStroke">
+                <div className="border-4 bubble bubbleTop">
+                    <h2>
+                        Hello World,
+                    </h2>
+                    <h2 >
+                        I'm Lorenzo
+                    </h2>
+                </div>
+            </div>
+            <div className="descDiv textStroke">
+                <div className="border-4 bubble bubbleBot">
+                    <p>
+                        Developer located in Orlando, FL
+                    </p>
+                </div>
+            </div>   
         </div>
         <h1>Vite + React</h1>
         <div className="card">
+            
+            <div className="flex justify-center">
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
             <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
             </button>
